306d49b2ebba4c114c316d2470622e169f72448c
jareal-loyalty <jareal@loyalty.com>
2024-10-26 01:25:39 -0300

    routing

---
+++ src/pages/CreateContact.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateContact: React.FC = () => {
+  return (
+    <div>
+      <h1>Create Contact Page</h1>
+    </div>
+  );
+};
+
+export default CreateContact;
